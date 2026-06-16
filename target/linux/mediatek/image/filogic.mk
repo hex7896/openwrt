37fb32010a1441d148ad6ad638928f1ebf29a1d5
hex7896 <144613635+hex7896@users.noreply.github.com>
2026-06-16 10:06:57 +0800

    Add DTS and modify filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1555,6 +1555,30 @@ endif
 endef
 TARGET_DEVICES += dlink_aquila-pro-ai-m60-a1
 
+define Device/dlink_dir-x6080z
+  DEVICE_VENDOR := D-Link
+  DEVICE_MODEL := DIR-X6080Z
+  DEVICE_DTS := mt7986a-dlink-dir-x6080z
+  DEVICE_DTS_DIR := ../dts
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware kmod-usb3
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7986-bl2 spim-nand-ddr4
+  ARTIFACT/bl31-uboot.fip := mt7986-bl31-uboot dlink_dir-x6080z
+endef
+TARGET_DEVICES += dlink_dir-x6080z
+
 define Device/edgecore_eap111
   DEVICE_VENDOR := Edgecore
   DEVICE_MODEL := EAP111
